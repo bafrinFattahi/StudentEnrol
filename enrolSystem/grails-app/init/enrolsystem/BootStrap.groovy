432162ef7ce0600afa3f1a09d731cffa456c9b29
bafrinFattahi <b6021259@my.shu.ac.uk>
2018-01-17 13:18:57 +0000

    Added data samples for Student objects

--- enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -31,15 +31,37 @@ description:'lOREM IPSUM.').save()
 
 def GameDevelopment=new Course(
 department:'Computing', 
-code: 'CS12444',
-title: 'BSs Hon Game Development',
-leader:'Dr john smith',
+code: 'CS34',
+title: 'BSs Hon NGame Development',
+leader:'Dr john Smith',
 startDate:new Date('09/09/2018'),
 endDate:new Date('07/07/2018'),
-numberOfStudents:60,
+numberOfStudents:55,
 studyMode:'FullTime',
 tutionFees:9000.00,
 description:'lOREM IPSUM.').save()
+
+
+
+def JSmith=new Student(
+name:'John Smith',
+studentID:'26021244',
+dob:new Date('24/09/1988'),
+isFundingAvailable: true,
+email:'Jsmith@student.com',
+username:'b6021244',
+password:'Bru123',
+course: 'BSc Computing').save()
+
+def Pdowson=new Student(
+name:'Peter Dawson',
+studentID:'26021233',
+dob:new Date('24/08/1989'),
+isFundingAvailable: true,
+email:'Pdawson@student.com',
+username:'b6021233',
+password:'Bru324',
+course: 'BSc Computing').save()
     }
     def destroy = {
     }
